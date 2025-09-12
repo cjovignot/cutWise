8e1dd8a8854226335be75c9f6ee02fb4e907af38
cjovignot <cosme.jovignot@epitech.eu>
2025-09-12 16:05:00 +0200

    Config vercel monorepo

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import userRoutes from "./api/users.js";
-import uploadRoutes from "./api/upload.js";
 
 dotenv.config();
 
@@ -11,16 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+// routes
+import userRoutes from "./api/users.js";
+import uploadRoutes from "./api/upload.js";
 
 app.use("/api/users", userRoutes);
 app.use("/api/upload", uploadRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
