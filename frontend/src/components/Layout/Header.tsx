c69342290c667bade432e3666cb0b932b820cc8d
cjovignot <cosme.jovignot@epitech.eu>
2025-09-12 13:58:11 +0200

    [INIT]

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -10,6 +10,7 @@ interface HeaderProps {
 export default function Header({ pageName, icon: Icon }: HeaderProps) {
   const { t } = useTranslation();
   const placeholder = t("search");
+  // test
 
   return (
     <header className="flex items-center justify-between w-full px-3 py-2 md:p-3">
