f035131174f9b5772515c29fb6a5869a35c7f088
cjovignot <cosme.jovignot@epitech.eu>
2025-09-13 03:26:21 +0200

    [FIX] Test increment version11

---
+++ frontend/update-version.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+// Chemin vers package.json
+const pkg = require("../package.json"); // ou "./frontend/package.json" selon ton repo
+
+// Chemin vers version.ts
+const versionFile = path.resolve(__dirname, "../src/version.ts");
+
+// Contenu à écrire
+const content = `export const APP_VERSION = "${pkg.version}";\n`;
+
+// Écriture
+fs.writeFileSync(versionFile, content);
+
+console.log(`✅ version.ts mise à jour : ${pkg.version}`);
