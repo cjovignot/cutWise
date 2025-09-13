de975e795b2351a933fa34245afc3f92e394795b
cjovignot <cosme.jovignot@epitech.eu>
2025-09-13 16:23:22 +0200

    [WIP] Dashboard Cards

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,7 +2,9 @@ import { useTranslation } from "react-i18next";
 
 function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div className="p-4 bg-white border shadow rounded-2xl">{children}</div>
+    <div className="flex flex-col p-4 bg-white shadow justify-evenly min-h-36 dark:bg-white/20 rounded-3xl">
+      {children}
+    </div>
   );
 }
 
@@ -15,18 +17,21 @@ function CardTitle({ children }: { children: React.ReactNode }) {
 }
 
 function CardContent({ children }: { children: React.ReactNode }) {
-  return <div className="text-sm">{children}</div>;
+  return <div className="text-xs">{children}</div>;
+}
+function CardFooter({ children }: { children: React.ReactNode }) {
+  return <div className="text-xs">{children}</div>;
 }
 
 export default function Dashboard() {
   const { t } = useTranslation();
 
   return (
-    <div className="p-6">
-      <h2 className="mb-6 text-2xl font-bold">{t("dashboard.title")}</h2>
+    <div className="">
+      {/* <h2 className="mb-6 text-2xl font-bold">{t("dashboard.title")}</h2> */}
 
       {/* Grid responsive */}
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-2 gap-3 md:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <Card>
           <CardHeader>
             <CardTitle>
@@ -35,10 +40,12 @@ export default function Dashboard() {
           </CardHeader>
           <CardContent>
             <p className="text-3xl font-bold">123</p>
+          </CardContent>
+          <CardFooter>
             <p className="text-muted-foreground">
               {t("dashboard.dashboard_cards.account_stats.count_users")}
             </p>
-          </CardContent>
+          </CardFooter>
         </Card>
 
         <Card>
@@ -49,10 +56,12 @@ export default function Dashboard() {
           </CardHeader>
           <CardContent>
             <p className="text-3xl font-bold">45</p>
+          </CardContent>
+          <CardFooter>
             <p className="text-muted-foreground">
               {t("dashboard.dashboard_cards.projects_stats.onGoingProjects")}
             </p>
-          </CardContent>
+          </CardFooter>
         </Card>
 
         <Card>
@@ -62,11 +71,13 @@ export default function Dashboard() {
             </CardTitle>
           </CardHeader>
           <CardContent>
-            <p className="text-3xl font-bold">€12,340</p>
+            <p className="text-3xl font-bold">12,34 €</p>
+          </CardContent>
+          <CardFooter>
             <p className="text-muted-foreground">
               {t("dashboard.dashboard_cards.revenue_stats.current_month")}
             </p>
-          </CardContent>
+          </CardFooter>
         </Card>
       </div>
     </div>
